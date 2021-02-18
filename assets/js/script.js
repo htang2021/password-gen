a71938fb643901599a1b1c3ee85bf2116d678292
Hung Tang <hungtang@hotmail.com>
2021-02-18 01:28:38 -0800

    modularized as much as possible - code is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,134 +1,185 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Prompt users for password criteria in order to generate password
 var generatePassword = function () {
-  //passLength();
-  //yesUpper();
-  //yesLower();
-  //yesNumber();
-  //yesSpChar();
-}
-
-// Write password to the #password input
-function writePassword() {
-  console.log("Function is called after 'click'.");
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-//Prompt for password length
-var passLength = function() {
-  var pwLength = prompt("How long do you want your password to be, between 8 and 128 in length?");
-  
-  pwLength = parseInt(pwLength);
-
-  if (pwLength < 8 || pwLength > 128) {
-    alert("You did not enter the appropriate length for your password.\n" +
-    "Please enter a length that's between 8 and 128.");
-    passLength();
-  } else if (isNaN(pwLength)){
-    alert("You have entered a non numeric value.  Please try again!");
-    passLength();
-  } else {
-    return pwLength;
+    var pwLength = passwordLength();
+    var needLower = yesLower();
+    var needUpper = yesUpper();
+    var needNumber = yesNumber();
+    var needSpChar = yesSpChar();
+    checkNumOfCharType(needLower, needUpper, needNumber, needSpChar);
+    var psWord = "";
+    for (i=0; i < pwLength; i++) {
+        if (needLower) {
+            psWord += genLowerCaseLetter();
+            needLower = false;
+        } else if (needUpper) {
+            psWord += genCapLetter();
+            needUpper = false;
+        } else if (needNumber) {
+            psWord += genRandomNum();
+            needNumber = false;
+        } else if (needSpChar) {
+            psWord += genSpecialChar();
+            needSpChar = false;
+        } else {
+            psWord += genGenericChar();
+        }
+    }
+    return psWord; 
   }
-}
-
-//Prompt to include UPPERCASE letters
-var yesUpper = function() {
-  var includeUpperCase = prompt("Do you want to include UPPER CASE letters in your password? \n" +
-  "Please enter 1 for YES; 2 for NO.");
-
-  includeUpperCase = parseInt(includeUpperCase);
-
-  debugger;
-
-  console.log(includeUpperCase);
   
-  if (includeUpperCase != 1 || includeUpperCase != 2) {
-    alert("You have entered an incorrect value.  Please try again.");
-    yesUpper();
-  } else if (includeUpperCase === 1) {
-    includeUpperCase = "true";
-    console.log(includeUpperCase);
-    return includeUpperCase;
-  } else {
-    includeUpperCase = "false";
-    console.log(includeUpperCase);
-    return includeUpperCase;
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
   }
+  
+
+/********** Gathering User Input **************************/
+/*****************************************************/
+
+//Prompt for length of password and validate entry
+var passwordLength = function () {
+    var pwLength = prompt("How long do you want your password to be, between 8 and 128 in length?");
+    pwLength = parseInt(pwLength);
+    if (pwLength < 8 || pwLength > 128) {
+        alert("You did not enter the appropriate length for your password.\n" +
+        "Please enter a length that's between 8 and 128.");
+        passwordLength();
+    } else if (isNaN(pwLength)) {
+        alert("You have entered a non numeric value.  Please try again!");
+        passwordLength();
+    } else {
+        return pwLength;
+    }
 }
 
-//Prompt to include lowerCase letters
+// Prompt to include Upper Case letter and validate entry
+var yesUpper = function () {
+    var includeUpperCase = prompt("Do you want to include UPPER CASE letters in your password? \n" +
+    "Please enter 1 for YES; 0 for NO.");
+    includeUpperCase = parseInt(includeUpperCase);
+
+    if (includeUpperCase != 1 && includeUpperCase != 0) {
+        alert("You have entered an incorrect value.  Please try again.");
+        yesUpper();
+    } else if (includeUpperCase) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Prompt to include lower case letter and validate entry
 var yesLower = function() {
-  var includeLowerCase = prompt("Do you want to include lower CASE letters in your password? \n" +
-  "Please enter 1 for YES; 2 for NO.");
+    var includeLowerCase = prompt("Do you want to include lower CASE letters in your password? \n" +
+    "Please enter 1 for YES; 0 for NO.");
+    includeLowerCase = parseInt(includeLowerCase);
 
-  includeLowerCase = parseInt(includeLowerCase);
-
-  if (includeLowerCase != 1 || includeLowerCase != 2) {
-    alert("You have entered an incorrect value.  Please try again.");
-    yesLower();
-  } else if (includeLowerCase === 1) {
-    includeLowerCase = "true";
-    return includeLowerCase;
-  } else {
-    includeLowerCase = "false";
-    return includeLowerCase;
-  }
+    if (includeLowerCase != 0 && includeLowerCase != 1) {
+        alert("You have entered an incorrect value.  Please try again.");
+        yesLower();
+    } else if (includeLowerCase) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-//Prompt to include numbers
+//Prompt to include numeric value and validate entry
 var yesNumber = function() {
-  var includeNum = prompt("Do you want to include numeric values in your password? \n" +
-  "Please enter 1 for YES; 2 for NO.");
+    var includeNum = prompt("Do you want to include numeric values in your password? \n" +
+    "Please enter 1 for YES; 0 for NO.");
+    includeNum = parseInt(includeNum);
 
-  includeNum = parseInt(includeNum);
-
-  if (includeNum != 1 || includeNum != 2) {
-    alert("You have entered an incorrect value.  Please try again.");
-    yesNumber();
-  } else if (includeNum === 1) {
-    includeNum = "true";
-    return includeNum;
-  } else {
-    includeNum = "false";
-    return includeNum;
-  }
+    if (includeNum != 1 && includeNum != 0) {
+        alert("You have entered an incorrect value.  Please try again.");
+        yesNumber();
+    } else if (includeNum) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-//Prompt to include special characters
+//Prompt to include special character and validate entry
 var yesSpChar = function() {
-  var includeSpChar = prompt("Do you want to include numeric values in your password? \n" +
-  "Please enter 1 for YES; 2 for NO.");
+    var includeSpChar = prompt("Do you want to include special characters in your password? \n" +
+    "Please enter 1 for YES; 0 for NO.");
+    includeSpChar = parseInt(includeSpChar);
 
-  includeSpChar = parseInt(includeSpChar);
+    if (includeSpChar != 1 && includeSpChar != 0) {
+        alert("You have entered an incorrect value.  Please try again.");
+        yesSpChar();
+    } else if (includeSpChar) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-  if (includeSpChar != 1 || includeSpChar != 2) {
-    alert("You have entered an incorrect value.  Please try again.");
-    yesSpChar();
-  } else if (includeSpChar === 1) {
-    includeSpChar = "true";
-    return includeSpChar;
-  } else {
-    includeSpChar = "false";
-    return includeSpChar;
-  }
+//Check if at least one character type is selected
+var checkNumOfCharType = function (needLower, needUpper, needNumber, needSpChar) {
+    if (needLower === false && needUpper === false && needNumber === false && needSpChar === false) {
+        alert("You need at least one character type in your password. \n" +
+        "Please start over!");
+        generatePassword();
+    }
+    return;
 }
 
 
-var passwordRule = {
-  pwLength: passLength(),
-  pwUpperCase: yesUpper(),
-  pwLowerCase: yesLower(),
-  pwNumber: yesNumber(),
-  pwSpChar: ['!', '@', '#', '$', '%', '&', '*'],
-};
+/**** Generate random character out of a string argument *******/
+/***************************************************************/
 
-// Add event listener to generate button
+var randomOut = function (stringArg) {
+    var stringIndex = Math.floor(Math.random() * stringArg.length);
+    return stringArg[stringIndex];
+}
+
+/********** Generate alphanumeric & special characters ********/
+/**************************************************************/
+
+//Generate a random Upper case letter
+var genCapLetter = function() {
+    const capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let aCapLetter = randomOut(capLetters);
+    return aCapLetter;
+}
+
+//Generate a random lower case letter
+var genLowerCaseLetter = function () {
+    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+    let aLowerCaseLetter = randomOut(lowerCaseLetters);
+    return aLowerCaseLetter;
+}
+
+//Generate a random number between 0-9
+var genRandomNum = function () {
+    const ranNum = "0123456789"
+    let aRandomNum = randomOut(ranNum);
+    return aRandomNum;
+}
+
+//Generate a random special character - ! @ # $ %  & * ?
+var genSpecialChar = function () {
+    const spChar = "!@#$%&*?";
+    let aSpecialChar = randomOut(spChar);
+    return aSpecialChar;
+}
+
+//Generate random trailing character after required character type is generated
+var genGenericChar = function () {
+    const genericChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*?";
+    let aGenericChar = randomOut(genericChar);
+    return aGenericChar;
+}
+
+
+// Add event listener to generate button ******************
+/**********************************************************/
 generateBtn.addEventListener("click", writePassword);
 
